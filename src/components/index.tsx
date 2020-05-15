e17118bac9e7d5b88204013bd4dc8f7416446d53
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-15 17:14:29 +0300

    checkpoint

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,13 +3,15 @@ import React, {Component} from 'react';
 import Bg from './bg';
 import UserForm from './user-form';
 
+import 'typeface-ibm-plex-sans';
 import '../style/style.scss';
+
 
 export default class AppPage extends Component {
     render() {
         return <>
             <Bg/>
-            <UserForm />
+            <UserForm/>
         </>
     }
 }
--- src/components/user-form/index.tsx
+++ src/components/user-form/index.tsx
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 
+import {Button} from 'react-bootstrap';
+
+import plusSvg from '../../images/plus.svg';
+
 export default class UserForm extends Component {
     render() {
         return (
-            <div className="upload-form">
-
+            <div className="user-form">
+                <div className="upload-form">
+                    <div className="form-controls">
+                        <div className="button-control">
+                            <Button variant="primary" size="sm">
+                                <img src={plusSvg} alt="Plus"/>
+                            </Button>
+                        </div>
+                        <div className="text-controls">
+                            <div className="select-file">
+                                Add files
+                            </div>
+                            <div className="select-folder">
+                                Or select a folder
+                            </div>
+                        </div>
+                    </div>
+                    <div className="upload-button">
+                        <Button variant="secondary" disabled>Upload</Button>
+                    </div>
+                </div>
             </div>
         )
     }
