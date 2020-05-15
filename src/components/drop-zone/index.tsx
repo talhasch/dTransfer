4a173e4eb634c2ba5000406d0ac651f9397d9e53
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-15 20:31:26 +0300

    checkpoint

---
+++ src/components/drop-zone/index.tsx
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+
+export default class DropZone extends Component {
+    componentDidMount(): void {
+        const root = document.querySelector('#root');
+        if (!root) return;
+        root.addEventListener('dragenter', this.dragEnter);
+    }
+
+    componentWillUnmount(): void {
+        const root = document.querySelector('#root');
+        if (!root) return;
+        root.removeEventListener('dragenter', this.dragEnter);
+    }
+
+    bringToFront = () => {
+        const el = document.querySelector('.drop-zone');
+        if (el) {
+            el.classList.add('visible');
+        }
+    };
+
+    sentToBack = () => {
+        const el = document.querySelector('.drop-zone');
+        if (el) {
+            el.classList.remove('visible');
+        }
+    };
+
+    dragEnter = () => {
+        this.bringToFront();
+    };
+
+    dragLeave = () => {
+        this.sentToBack();
+    };
+
+    dragOver = (e: React.DragEvent) => {
+        e.dataTransfer.effectAllowed = 'copy';
+        e.dataTransfer.dropEffect = 'copy';
+        e.preventDefault();
+    };
+
+    drop = async (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.sentToBack();
+
+        const {files} = e.dataTransfer;
+
+        if (files.length === 0) {
+            return;
+        }
+
+        console.log(files)
+    };
+
+    render() {
+        return <div className="drop-zone"
+                    onDrop={this.drop}
+                    onDragLeave={this.dragLeave}
+                    onDragOver={this.dragOver}>
+            Drop files here
+        </div>;
+    }
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,60 +2,18 @@ import React, {Component} from 'react';
 
 import Bg from './bg';
 import UserForm from './user-form';
+import DropZone from './drop-zone';
 
 import 'typeface-ibm-plex-sans';
+
 import '../style/style.scss';
 
-
 export default class AppPage extends Component {
-
-    highlight = () => {
-        const el = document.querySelector('.wrapper');
-        if (el) {
-            el.classList.add('highlighted');
-        }
-    };
-
-    unHighlight = () => {
-        const el = document.querySelector('.wrapper');
-        if (el) {
-            el.classList.remove('highlighted');
-        }
-    };
-
-    dragEnter = () => {
-        this.highlight();
-    };
-
-    dragLeave = () => {
-        this.unHighlight();
-    };
-
-    dragOver = (e: React.DragEvent) => {
-        e.dataTransfer.effectAllowed = 'copy';
-        e.dataTransfer.dropEffect = 'copy';
-        e.preventDefault();
-    };
-
-
-    drop = async (e: React.DragEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        this.unHighlight();
-
-        const {files} = e.dataTransfer;
-
-        if (files.length === 0) {
-            return;
-        }
-        console.log(files)
-    };
-
     render() {
-        return <div className="wrapper" onDragEnter={this.dragEnter} onDragOver={this.dragOver}>
-            <div className="drop-zone" onDrop={this.drop} onDragLeave={this.dragLeave}></div>
-            <Bg/>
-            <UserForm/>
-        </div>
+        return <>
+            <DropZone {...this.props} />
+            <Bg {...this.props} />
+            <UserForm {...this.props} />
+        </>
     }
 }
