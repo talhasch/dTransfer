33e2bb4b9efc5175dd38db13e447c852cabb330e
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-15 19:18:47 +0300

    checkpoint

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,10 +8,54 @@ import '../style/style.scss';
 
 
 export default class AppPage extends Component {
+
+    highlight = () => {
+        const el = document.querySelector('.wrapper');
+        if (el) {
+            el.classList.add('highlighted');
+        }
+    };
+
+    unHighlight = () => {
+        const el = document.querySelector('.wrapper');
+        if (el) {
+            el.classList.remove('highlighted');
+        }
+    };
+
+    dragEnter = () => {
+        this.highlight();
+    };
+
+    dragLeave = () => {
+        this.unHighlight();
+    };
+
+    dragOver = (e: React.DragEvent) => {
+        e.dataTransfer.effectAllowed = 'copy';
+        e.dataTransfer.dropEffect = 'copy';
+        e.preventDefault();
+    };
+
+
+    drop = async (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.unHighlight();
+
+        const {files} = e.dataTransfer;
+
+        if (files.length === 0) {
+            return;
+        }
+        console.log(files)
+    };
+
     render() {
-        return <>
+        return <div className="wrapper" onDragEnter={this.dragEnter} onDragOver={this.dragOver}>
+            <div className="drop-zone" onDrop={this.drop} onDragLeave={this.dragLeave}></div>
             <Bg/>
             <UserForm/>
-        </>
+        </div>
     }
 }
