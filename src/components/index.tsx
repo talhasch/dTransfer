6768f38d4b514244c0fc21df2a5e02ee3a16258d
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-15 16:45:06 +0300

    checkpoint

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Bg from './bg';
+import UserForm from './user-form';
 
 import '../style/style.scss';
 
@@ -8,6 +9,7 @@ export default class AppPage extends Component {
     render() {
         return <>
             <Bg/>
+            <UserForm />
         </>
     }
 }
---
+++ src/components/user-form/index.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class UserForm extends Component {
+    render() {
+        return (
+            <div className="upload-form">
+
+            </div>
+        )
+    }
+}
