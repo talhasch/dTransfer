9b4f6a0e09b4a9785d777feff7b46504b0df45d5
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-16 14:03:10 +0300

    checkpoint

--- src/components/transfer/queue.tsx
+++ src/components/transfer/queue.tsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+
+import fileSize from 'filesize';
+
 import {State as UploadQueueState} from '../../store/upload-queue/types';
 
 interface QueueProps {
@@ -17,10 +20,10 @@ export default class Queue extends Component<QueueProps> {
         return <div className="queue">
             {list.map((i) => {
                 return <div key={i.id} className="item">
-                    <div className="item-info">
-                        <div className="item-name">
-                            {i.obj.name}
-                        </div>
+                    <div className="item-name">{i.obj.name}</div>
+                    <div className="item-size">{fileSize(i.obj.size)}</div>
+                    <div className="item-controls">
+
                     </div>
                 </div>
             })}
