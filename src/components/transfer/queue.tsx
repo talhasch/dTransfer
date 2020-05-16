cfe45f58ea357c2b752ce88b1732f8c62e721e31
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-17 01:12:33 +0300

    checkpoint

--- src/components/transfer/queue.tsx
+++ src/components/transfer/queue.tsx
@@ -4,7 +4,7 @@ import fileSize from 'filesize';
 
 import {Item, ItemStatus, State as UploadQueueState} from '../../store/upload-queue/types';
 
-import {closeSvg} from '../../svg';
+import {closeSvg, checkSvg} from '../../svg';
 
 interface QueueProps {
     deleteUploadQueueItem: (id: string) => any
@@ -27,14 +27,17 @@ export default class Queue extends Component<QueueProps> {
 
         return <div className="queue">
             {list.map((i) => {
-                const progress = `${i.progress}%`;
-                return <div key={i.id} className="item">
+                const progress = i.status === ItemStatus.IN_PROGRESS ? `${i.progress}%` : '0px';
+
+                return <div key={i.id} className={`item ${i.status === ItemStatus.IN_PROGRESS ? 'in-progress' : ''}`}>
                     <div className="item-progress" style={{width: progress}}/>
                     <div className="item-content">
-                        <div className="item-name">{i.obj.name}</div>
-                        <div className="item-size">{fileSize(i.obj.size)}</div>
+                        <div className="item-info">
+                            <div className="name">{i.obj.name}</div>
+                            <span className="size">{fileSize(i.obj.size)}</span>
+                            {(i.status === ItemStatus.IN_PROGRESS && i.progress === 100) && <span className="processing">processing</span>}
+                        </div>
                         <div className="item-controls">
-
                             {(() => {
                                 if (i.status === ItemStatus.READY) {
                                     if (!inProgress) {
@@ -46,14 +49,8 @@ export default class Queue extends Component<QueueProps> {
                                     }
                                 }
 
-                                if (i.status === ItemStatus.IN_PROGRESS) {
-                                    if (i.progress === 100) {
-                                        return "processing"
-                                    }
-                                }
-
                                 if (i.status === ItemStatus.DONE) {
-
+                                    return <div className="item-control">{checkSvg}</div>
                                 }
 
                                 if (i.status === ItemStatus.ERROR) {
@@ -62,7 +59,6 @@ export default class Queue extends Component<QueueProps> {
 
                                 return null;
                             })()}
-
                         </div>
                     </div>
                 </div>
